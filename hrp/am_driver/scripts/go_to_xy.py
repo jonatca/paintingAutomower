0758e96287e0ba37bd72e49786c91d5fc163fb77
jonatca <jonathan.carlson01@outlook.com>
2023-03-11 13:54:48 +0100

    optimized regulator parameters

--- hrp/am_driver/scripts/go_to_xy.py
+++ hrp/am_driver/scripts/go_to_xy.py
@@ -1,15 +1,15 @@
 import numpy as np
 import math
 
-max_lin_vel = 0.2
+max_lin_vel = 0.3
 max_ang_vel = 0.8
-min_z_vel = 0.2
+# min_z_vel = 0.01
 start_slow_lin = 15 * np.pi / 180  # starts to slow down when error less than this
 start_slow_ang = 0.5
-kp_linear = 0.7  # these are tested to be good, not sure if they are the best
-kp_angular = 0.7
-tolerance_position = 0.005  # meters
-tolerance_angle = 3 * np.pi / 180
+kp_linear = 1  # these are tested to be good, not sure if they are the best
+kp_angular = 0.5
+tolerance_position = 0.01  # meters
+tolerance_angle = 5 * np.pi / 180
 
 
 def calc_vel(z_dir, w_dir, x, y, x_goal, y_goal, update_freq):
@@ -17,19 +17,18 @@ def calc_vel(z_dir, w_dir, x, y, x_goal, y_goal, update_freq):
     error_distance = math.sqrt((x_goal - x) ** 2 + (y_goal - y) ** 2)
     print("error_distance=", error_distance, "error_angle=", error_angle)
     if error_distance > tolerance_position:
-        x_vel = 0
+        z_vel = _calc_z_vel(error_angle, error_distance)
         if abs(error_angle) > tolerance_angle:
-            z_vel = _calc_z_vel(error_angle, error_distance)
             x_vel = 0
         else:
             x_vel = max_lin_vel * np.sign(error_distance)
             if error_distance < start_slow_lin:
                 x_vel = kp_linear * max_lin_vel * error_distance / start_slow_lin
             x_vel = min(np.abs(x_vel), np.abs(max_lin_vel))  # just in case
-            z_vel = 0
     else:
         x_vel = 0
         z_vel = 0
+
     print("x_vel=", x_vel, "z_vel=", z_vel)
     return x_vel, z_vel
 
@@ -40,10 +39,10 @@ def _calc_z_vel(error_angle, error_distance):
         z_vel = kp_angular * max_ang_vel * error_angle / start_slow_ang
     # elif error_distance < start_slow_lin:  # when close rotate
     #     z_vel = kp_angular * max_ang_vel * np.abs(error_distance) / error_distance
-    if np.abs(z_vel) < np.abs(min_z_vel):
-        z_vel = np.sign(z_vel) * min_z_vel
+    # if np.abs(z_vel) < np.abs(min_z_vel):
+    #     z_vel = np.sign(z_vel) * min_z_vel
     if np.abs(z_vel) > np.abs(max_ang_vel):
-        z_ve = np.sign(z_vel) * max_ang_vel  # limit for safty reasons
+        z_vel = np.sign(z_vel) * max_ang_vel  # limit for safty reasons
     return z_vel
 
 
--- hrp/am_driver/scripts/main.py
+++ hrp/am_driver/scripts/main.py
@@ -7,21 +7,39 @@ from geometry_msgs.msg import PoseStamped
 import numpy as np
 from go_to_xy import calc_vel
 
-go_to_x_pos = 0
+go_to_x_pos = 1
 go_to_y_pos = 0
 update_freq = 10
 
 
-def pose_callback(pose):
-    x = pose.pose.position.x  # get x position
-    y = pose.pose.position.y  # get y position
-    # rospy.loginfo("Automower position: x=%s, y=%s", x, y)
-    z_dir = pose.pose.orientation.z  # get z direction
-    w_dir = pose.pose.orientation.w  # get w direction
+reached_goal = False
 
-    x_vel, z_vel = calc_vel(z_dir, w_dir, x, y, go_to_x_pos, go_to_y_pos, update_freq)
-    twist.linear.x = x_vel
-    twist.angular.z = z_vel
+
+def pose_callback(pose):
+    global reached_goal
+    if not reached_goal:
+        x = pose.pose.position.x  # get x position
+        y = pose.pose.position.y  # get y position
+        # rospy.loginfo("Automower position: x=%s, y=%s", x, y)
+        z_dir = pose.pose.orientation.z  # get z direction
+        w_dir = pose.pose.orientation.w  # get w direction
+
+        x_vel, z_vel = calc_vel(
+            z_dir, w_dir, x, y, go_to_x_pos, go_to_y_pos, update_freq
+        )
+        twist.linear.x = x_vel
+        twist.angular.z = z_vel
+        if x_vel == 0 and z_vel == 0:
+            twist.linear.x = x_vel
+            twist.angular.z = z_vel
+            # turn off program
+
+            # rospy.is_shutdown()
+            # rospy.loginfo(
+            #     "Automower has reached position x=%s, y=%s", go_to_x_pos, go_to_y_pos
+            # )
+            reached_goal = True
+            rospy.signal_shutdown("Reached goal position")
 
 
 rospy.init_node("move_forward")
@@ -33,7 +51,7 @@ twist = Twist()
 start_time = rospy.Time.now()
 duration = rospy.Duration(15)
 
-while not rospy.is_shutdown():
+while not rospy.is_shutdown() and not reached_goal:
     current_time = rospy.Time.now()
     elapsed_time = current_time - start_time
     if elapsed_time < duration:
