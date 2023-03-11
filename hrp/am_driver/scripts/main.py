efc21edfa53323e8412350a3b9bd4f7d4850a429
jonatca <jonathan.carlson01@outlook.com>
2023-03-11 15:12:13 +0100

    made main prettier

--- hrp/am_driver/scripts/main.py
+++ hrp/am_driver/scripts/main.py
@@ -11,12 +11,43 @@ from go_to_xy import (
     calc_vel,
 )  # error? is go_to_xy.pyc in calculations folder? try chmod 777 go_to_xy.py and catkin_make
 
-go_to_x_pos = 0.5
+go_to_x_pos = 1
 go_to_y_pos = 0
 update_freq = 10
 duration = 10
-
 reached_goal = False
+twist = Twist()
+
+
+def main():
+    global elapsed_time
+    rospy.init_node("move_forward")
+    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    sub = rospy.Subscriber("/pose", PoseStamped, pose_callback)
+    rate = rospy.Rate(update_freq)
+
+    start_time = rospy.Time.now()
+    duration = rospy.Duration(15)
+
+    while not rospy.is_shutdown() and not reached_goal:
+        current_time = rospy.Time.now()
+        elapsed_time = current_time - start_time
+        if elapsed_time < duration:
+            pub.publish(twist)
+        else:
+            twist.linear.x = 0
+            twist.angular.z = 0
+            pub.publish(twist)
+            break
+        rate.sleep()
+
+    twist.linear.x = 0
+    twist.angular.z = 0
+    pub.publish(twist)
+
+    rospy.loginfo(
+        "Automower has moved to position x=%s, y=%s", go_to_x_pos, go_to_y_pos
+    )
 
 
 def pose_callback(pose):
@@ -42,29 +73,5 @@ def pose_callback(pose):
             reached_goal = True
 
 
-rospy.init_node("move_forward")
-pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-sub = rospy.Subscriber("/pose", PoseStamped, pose_callback)
-rate = rospy.Rate(update_freq)
-
-twist = Twist()
-start_time = rospy.Time.now()
-duration = rospy.Duration(15)
-
-while not rospy.is_shutdown() and not reached_goal:
-    current_time = rospy.Time.now()
-    elapsed_time = current_time - start_time
-    if elapsed_time < duration:
-        pub.publish(twist)
-    else:
-        twist.linear.x = 0
-        twist.angular.z = 0
-        pub.publish(twist)
-        break
-    rate.sleep()
-
-twist.linear.x = 0
-twist.angular.z = 0
-pub.publish(twist)
-
-rospy.loginfo("Automower has moved to position x=%s, y=%s", go_to_x_pos, go_to_y_pos)
+if __name__ == "__main__":
+    main()
