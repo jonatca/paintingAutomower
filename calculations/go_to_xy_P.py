64aa11284e257b89f81d1ad1f35ea2476bab8ae9
jonatca <jonathan.carlson01@outlook.com>
2023-03-13 16:00:42 +0100

    added offset, (1,0) = 1 m forward

--- calculations/go_to_xy_P.py
+++ calculations/go_to_xy_P.py
@@ -2,24 +2,22 @@ import numpy as np
 
 
 class P:
-    def __init__(self, x_start, y_start, x_goal, y_goal, update_freq):
+    def __init__(self, x_goal, y_goal, update_freq):
+        self.tol_lin = 0.01  # tolerance in meter
+        self.tol_ang = 5 * np.pi / 180
+        self.min_tol_ang = 0.1 * np.pi / 180  # to avoid calculations error
+        self.max_vel_lin = 0.3
+        self.max_vel_ang = 0.8
         self.Kp_l = 0.7
         self.Kp_a = 0.8
-        self.x_start = x_start
-        self.y_start = y_start
-        self.x_goal = x_goal
+        self.x_goal = x_goal  # global coordinates
         self.y_goal = y_goal
         self.update_time = 1.0 / update_freq
-        self.tol_lin = 0.05
-        self.tol_ang = 0.2
         self.current_ang = None
         self.error_lin = None
         self.vel_lin = None
         self.x = None
         self.y = None
-        self.prev_error_lin = 0
-        self.max_vel_lin = 0.3
-        self.max_vel_ang = 0.8
 
     def calc_vel(self, current_ang, x, y):
         self.current_ang = current_ang
@@ -28,15 +26,16 @@ class P:
         self.error_lin = np.sqrt(
             (self.x_goal - self.x) ** 2 + (self.y_goal - self.y) ** 2
         )
-        self.vel_lin = self.Kp_l * self.error_lin
-        self.vel_lin = np.clip(self.vel_lin, -self.max_vel_lin, self.max_vel_lin)
-        self.prev_error_lin = self.error_lin
-
-        # Calculate angular velocity
+        self.vel_lin = self.Kp_l * self.error_lin  # slows down when close to goal
+        self.vel_lin = np.clip(
+            self.vel_lin, -self.max_vel_lin, self.max_vel_lin
+        )  # limits max velocity
         self.goal_ang = np.arctan2(self.y_goal - self.y, self.x_goal - self.x)
         self.error_ang = self.goal_ang - self.current_ang
         self.error_ang = (self.error_ang + np.pi) % (2 * np.pi) - np.pi
-        self.vel_ang = self.Kp_a * self.error_ang
+        if self.error_lin < self.min_tol_ang:  # to avoid calculatins error
+            self.error_ang = 0
+        self.vel_ang = self.Kp_a * self.error_ang  # more error => more angular velocity
         self.vel_ang = np.clip(self.vel_ang, -self.max_vel_ang, self.max_vel_ang)
         if self.error_lin < self.tol_lin:  # dont move if close to goal
             self.vel_lin = 0
--- hrp/am_driver/scripts/main2.py
+++ hrp/am_driver/scripts/main2.py
@@ -6,23 +6,42 @@ import rospy
 import tf.transformations
 import signal
 from geometry_msgs.msg import Twist, PoseStamped
+import numpy as np
 import go_to_xy_P
 
 
 class Drive_to:
-    def __init__(self, x_goal, y_goal):
+    def __init__(self, x_goal_prim, y_goal_prim, reset_angle=True):
         self.update_freq = 10.0  # Hz but doesn't work? stuck at 10 Hz
-        self.x_goal = x_goal
-        self.y_goal = y_goal
+        self.x_goal_prim = x_goal_prim
+        self.y_goal_prim = y_goal_prim
         self.x = None
         self.y = None
+        self.x_start = None
+        self.y_start = None
+        self.init_angle = None
+        self.reset_angle = reset_angle
         self.twist = Twist()
-        self.pid = go_to_xy_P.P(0, 0, self.x_goal, self.y_goal, self.update_freq)
+
+        self.pid = None  # go_to_xy_P.P(self.x_goal, self.y_goal, self.update_freq)
         self.reached_goal = False
         rospy.init_node("move_forward")
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         sub = rospy.Subscriber("/pose", PoseStamped, self.pose_callback)
         self.rate = rospy.Rate(self.update_freq)
+
+    def goal_coords(self):  # prim coordinates => automowers relative coordinates
+        x_goal = (
+            self.x_start
+            + self.x_goal_prim * np.cos(self.init_angle)
+            - self.y_goal_prim * np.sin(self.init_angle)
+        )
+        y_goal = (
+            self.y_start
+            + self.x_goal_prim * np.sin(self.init_angle)
+            + self.y_goal_prim * np.cos(self.init_angle)
+        )
+        return x_goal, y_goal  # given in automowers global coordinates
 
     def drive(self):
         signal.signal(
@@ -41,11 +60,22 @@ class Drive_to:
         )
 
     def pose_callback(self, pose):
-        self.x = pose.pose.position.x
-        self.y = pose.pose.position.y
         z_dir = pose.pose.orientation.z
         w_dir = pose.pose.orientation.w
         current_ang = tf.transformations.euler_from_quaternion([0, 0, z_dir, w_dir])[2]
+        if (
+            self.reset_angle
+            and self.init_angle is None
+            and self.x_start is None
+            and self.y_start is None
+        ):
+            self.init_angle = current_ang
+            self.x_start = pose.pose.position.x
+            self.y_start = pose.pose.position.y
+            x_goal, y_goal = self.goal_coords()
+            self.pid = go_to_xy_P.P(x_goal, y_goal, self.update_freq)
+        self.x = pose.pose.position.x
+        self.y = pose.pose.position.y
         lin_vel, ang_vel = self.pid.calc_vel(current_ang, self.x, self.y)
         self.twist.linear.x = lin_vel
         self.twist.angular.z = ang_vel
@@ -56,11 +86,11 @@ class Drive_to:
         self.twist.linear.x = 0.0
         self.twist.angular.z = 0.0
         self.pub.publish(self.twist)
-        rospy.signal_shutdown("User interrupted")
+        rospy.signal_shutdown("User interrupted by ctrl+c")
 
 
 if __name__ == "__main__":
-    x_goal = 0.5
-    y_goal = 0.5
+    x_goal = 1  # positive x = forward
+    y_goal = 0  # positive y = left
     drive_to = Drive_to(x_goal, y_goal)
     drive_to.drive()
