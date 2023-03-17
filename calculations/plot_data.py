4043933174a4c75cd3e8f6ba34fea9a908d498c3
jonatca <jonathan.carlson01@outlook.com>
2023-03-17 13:06:07 +0100

    changed to python 2.7

--- calculations/plot_data.py
+++ calculations/plot_data.py
@@ -34,9 +34,10 @@ def plot_data():
     plt.legend()
     plt.axis("equal")
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    plt.savefig(f"plots/plot-{timestamp}.png")
-    plt.savefig(f"plots/plot-latest.png")
-    print(f"Saved plot to plots/plot-{timestamp}.png")
+    plt.savefig("plots/plot-", {timestamp}, ".png")
+    plt.savefig("plots/plot-latest.png")
+    print("Saved plot to plots/plot-", {timestamp}, ".png")
+
 
 if __name__ == "__main__":
     plot_data()
