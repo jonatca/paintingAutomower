3102ae0ca323d7d57ca27a671211d55f0a13afce
malmstromjr <gugge.malmstrom@gmail.com>
2023-03-28 15:01:01 +0200

    sss

--- calculations/kalman.py
+++ calculations/kalman.py
@@ -5,50 +5,65 @@ class EKF2D:
     def __init__(self, initial_state, initial_input, initial_covariance, process_noise, measurement_noise):
         self.state = initial_state   #state tre värden: x y theta
         self.input= initial_input  # två värden v  omega
-        self.covariance = initial_covariance
-        self.process_noise = process_noise
+        self.P_k = initial_covariance   # vi använder P_k istället för bara covariance
+        self.Q_k = process_noise    # state model noise, cov matrix
         self.measurement_noise = measurement_noise
-        
+        self.theta = initial_state[2] ### går det göra så?
+
+    def get_B(self, theta, dt): #räkna ut B matris från värden 
+         B = np.array([[np.cos(theta)*dt, 0],
+                      [np.sin(theta)*dt, 0
+                       [0, dt]]])
+         return B
     def predict(self, delta_x, delta_y, dt):
-        F = np.array([
-            [1, 0, dt, 0],
-            [0, 1, 0, dt],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ])
-        theta=0 #komma från state!!!!!!!!!
-        # [x(k), y(k), theta(k)]^T = A * [x(k-1), y(k-1),theta(k-1)]^T + B* [v(k-1), omega(k-1)] + noise
+                                                #F = np.array([
+                                                #    [1, 0, dt, 0],
+                                                #    [0, 1, 0, dt],
+                                                #    [0, 0, 1, 0],
+                                                #    [0, 0, 0, 1]
+                                                #])#gammal kod
+        
+        #  (1): [x(k), y(k), theta(k)]^T = A * [x(k-1), y(k-1),theta(k-1)]^T + B* [v(k-1), omega(k-1)] + noise
+
         A = np.array([[1,0,0],
                       [0,1,0],
                       [0,0,1]])
-        B = np.array([[np.cos(theta)*dt, 0],
-                      [np.sin(theta)*dt, 0
-                       [0, dt]]])
+        B = self.get_B(self.theta, dt)  #np.array([[np.cos(theta)*dt, 0],
+                                                # [np.sin(theta)*dt, 0
+                                                    #[0, dt]]])
         
         #u = np.array([delta_x, delta_y, delta_x, delta_y])
         # self.state = np.dot(A, self.state) + u
+        noise= np.array([[0],[0],[0]]) # ngt ryp av noise får läggas till
 
-        self.state = np.dot(A, self.state) + np.dot(B, self.input)
-        
-        self.covariance = np.dot(F, np.dot(self.covariance, F.T)) + self.process_noise
+        self.state = np.dot(A, self.state) + np.dot(B, self.input) + noise #beräkna state funktion med ekv (1)
+
+        self.P_k = np.dot(A, np.dot(self.P_k, A.T)) + self.Q_k
+        ################ predict Klar##############
     def get_state(self):
         return self.state
     
-    def update_gps(self, gps_x, gps_y):
+    def update_gps(self, gps_x, gps_y): #fixa funktion sen?
         return self.update(gps_x, gps_y, self.measurement_noise)
     
     def update(self, gps_x, gps_y, position_covariance):
-        H = np.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0]
+        self.R_k= position_covariance   #covariance matris för mätbara
+
+        H_k = np.array([
+            [1, 0, 0 ],
+            [0, 1, 0 ],
+            [0, 0, 1]
         ])
+        yaw =0  ## lägga in data för nya sensorn med vinkel
+        y_k = np.array([gps_x, gps_y, yaw]) - np.dot(H_k, self.state)   # Calculate the difference between the actual sensor measurements
+                                                                        # at time k minus what the measurement model prmeaedicted 
+                                                                        # the sensor measurements would be for the current timestep k.
 
-        y = np.array([gps_x, gps_y]) - np.dot(H, self.state)
-        S = np.dot(H, np.dot(self.covariance, H.T)) + position_covariance
-        K = np.dot(self.covariance, np.dot(H.T, np.linalg.inv(S)))
+        S_k = np.dot(H_k, np.dot(self.P_k, H_k.T)) + self.R_k           # R_k=position_covariance
+        K_k = np.dot(self.P_k, np.dot(H_k.T, np.linalg.inv(S_k)))       #np.linalg.pinv??
 
-        self.state = self.state + np.dot(K, y)
-        self.covariance = np.dot(np.eye(4) - np.dot(K, H), self.covariance)
+        self.state = self.state + np.dot(K_k, y_k)
+        self.P_k = self.P_k - np.dot(K_k, np.dot(H_k, self.P_k))        #uppdatera state covariance för tid k    # np.dot(np.eye(4) - np.dot(K_k, H_k), self.covariance)
 
 
 
