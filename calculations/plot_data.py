0cfe1a36c234203eada3af1526b6d77e257b68c4
Jonathan Carlson <jonathan.carlson01@outlook.com>
2023-03-31 08:59:59 +0200

    plot data update

--- calculations/plot_data.py
+++ calculations/plot_data.py
@@ -34,11 +34,11 @@ def convert_to_xy(lat, lon, lat_start, lon_start):
     transformer = pyproj.Transformer.from_crs(wgs84, sweref, always_xy=True)
     x_start, y_start = transformer.transform(lon_start, lat_start)
     x, y = transformer.transform(lon, lat)
-    x -= x_start
-    y -= y_start
+    # x -= x_start
+    # y -= y_start
 
-    #x = (lat - lat_start) * 111139
-    #y = (lon - lon_start) * 111139
+    # x = (lat - lat_start) * 111139
+    # y = (lon - lon_start) * 111139
     return x, y 
 
 def convert_to_xy2(lat_start, lon_start, lat, lon, x_start, y_start, angle_start):
@@ -56,13 +56,12 @@ def lat_lon_to_cartesian(lat, lon):
 def global_to_local(x_gps, y_gps, x_gps_start, y_gps_start, angle_north, x_start, y_start, angle_start):
     dx = x_gps - x_gps_start
     dy = y_gps - y_gps_start
-    angle_start *= 0
-    angle_diff = np.pi/4 # why does this work? 
-    angle_north -=  angle_diff
-    x_local = dx * math.cos(angle_north-angle_start) + dy * math.sin(angle_north-angle_start)
-    y_local = -dx * math.sin(angle_north-angle_start) + dy * math.cos(angle_north-angle_start)
+    # angle_start *= 0
+    # angle_diff = np.pi/4 # why does this work? 
+    # angle_north -=  angle_diff
+    x_local = x_start + dx * math.cos(angle_north-angle_start) + dy * math.sin(angle_north-angle_start)
+    y_local = y_start -dx * math.sin(angle_north-angle_start) + dy * math.cos(angle_north-angle_start)
     return x_local, y_local
-
 
 
 def plot_data(GPS=True, filename="data.json"):
@@ -79,8 +78,8 @@ def plot_data(GPS=True, filename="data.json"):
     y_goal = data["y_goal"]
     angle_start = np.arctan2(np.mean(y[0:70]) - y[0], np.mean(x[0:70]) - x[0])
     print(angle_start, "angle_start1")
-    for i in range(len(x)):
-        x[i],y[i] = inv_change_coord_sys(x[i], y[i] ,x_start, y_start, angle_start)
+    # for i in range(len(x)):
+        # x[i],y[i] = inv_change_coord_sys(x[i], y[i] ,x_start, y_start, angle_start)
     angle_start = np.arctan2(np.mean(y[0:70]) - y[0], np.mean(x[0:70]) - x[0])
     print(angle_start, "angle_start2")
     if GPS:
@@ -93,6 +92,8 @@ def plot_data(GPS=True, filename="data.json"):
         print(angle_start)
         for i in range(len(lat)):
             x_gps[i], y_gps[i] = convert_to_xy2(lat[i], lon[i], lat_start, lon_start, x_start, y_start, angle_start)
+            x_gps[i], y_gps[i] = convert_to_xy(lat[i], lon[i], lat_start, lon_start)
+            x_gps[i], y_gps[i] = lat_lon_to_cartesian(lat[i], lon[i])
     try:
         radius = data["radius"][0]
         x_mid = data["x_mid"][0]
@@ -102,11 +103,11 @@ def plot_data(GPS=True, filename="data.json"):
         plt.gca().add_patch(circle)
     except:
         pass
-    plt.plot(x, y, "o-", label="Path Ordometri", markersize=3)
-    plt.plot(x_goal, y_goal, "rx", markersize=3, label="Goal")
+    # plt.plot(x, y, "o-", label="Path Ordometri", markersize=3)
+    # plt.plot(x_goal, y_goal, "rx", markersize=3, label="Goal")
     if GPS:
         plt.plot(x_gps, y_gps, "o-", label="Path GPS", markersize=1)
-
+    print(np.arctan2(np.mean(x_gps[0:70]) - y[0], np.mean(y_gps[0:70]) - x[0]), "angle_start3")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.title("Path and Goal")
@@ -120,4 +121,4 @@ def plot_data(GPS=True, filename="data.json"):
 
 
 if __name__ == "__main__":
-    plot_data(GPS = True, filename="../data/motnorr.json")
+    plot_data(GPS = True, filename="../data/motost.json")
