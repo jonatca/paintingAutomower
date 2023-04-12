631aeeaf9cf77ded7c91df46fc1f4859b9fb188d
malmstromjr <gugge.malmstrom@gmail.com>
2023-04-12 11:43:04 +0200

    skkrrr

--- calculations/lqr.py
+++ calculations/lqr.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import control as ct
 class LQR:
     def __init__(self, initial_state, initial_input):
         self.state = initial_state   #state tre värden: x y theta
@@ -33,10 +33,32 @@ class LQR:
         
         state_error = self.state - desired_state
 
+        #### chatgpt säger detta ####
+
+        # Compute LQR gain matrix
+        K, _, _ = ct.lqr(A, B, Q, R)
+
+        u = -np.dot(K, state_error) 
+
+        #### istället för att skriva ut ekvationerna nedan ####   
+
+
         N = 50  # man kan testa olika här
         P = [None]*(N+1)
         Qf = Q
         P[N] = Qf
 
         for i in range(N, 0, -1):
-            P[i-1] = Q + np.dot(A.T, np.dot(P[i], A)) - (np.dot(A.T, np.dot(P[i],B))) #fortsätt
+            #P[i-1] = Q + A^T P[i]A – (A^T P[i]B)(R + BTP[i]B)^-1(B^T P[i]A)  
+            P[i-1] = Q + np.dot(A.T, np.dot(P[i], A)) - np.dot(np.dot(np.dot(A.T, np.dot(P[i],B)), np.linalg.inv(R+np.dot(B.T, np.dot(P[i], B)))), (np.dot(B.T, np.dot(P[i], A)))) 
+
+        K = [None]* N
+        u = [None]* N
+
+        for i in range(N):
+            # K[i] = -(R + B^T P[i+1]B)^-1 B^T P[i+1]A
+            K[i]= -np.dot(np.linalg.inv(R + np.dot(B.T, np.dot(P[i+1], B))), np.dot(B.T, np.dot(P[i+1], A))) ## Calculate the optimal feedback gain K
+            u[i] = np.dot(K[i], state_error)
+        u_star=u[N-1]  ## Optimal control input is u_star
+
+        return u_star  ## bara returna u om lqr från gpt funkar    
